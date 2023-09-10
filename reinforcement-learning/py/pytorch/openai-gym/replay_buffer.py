1fbd61ebdab685a8e9fffb2e258d137531cda74b
a-r-r-o-w <avs050602@gmail.com>
2023-09-10 11:10:46 +0530

    add q network notebooks

---
+++ reinforcement-learning/py/pytorch/openai-gym/replay_buffer.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+from typing import List, Tuple
+
+
+class ReplayBuffer:
+    def __init__(self, memory_size: int, input_shape: List[int]) -> None:
+        """
+        Initializes the ReplayBuffer.
+
+        Parameters
+        ----------
+        memory_size: int
+            The size of the replay buffer.
+        input_shape: List[int]
+            The shape of the input.
+        """
+
+        self.memory_size = memory_size
+        self.memory_counter = 0
+        self.state_memory = np.zeros((self.memory_size, *input_shape), np.float32)
+        self.next_state_memory = np.zeros((self.memory_size, *input_shape), np.float32)
+        self.action_memory = np.zeros(self.memory_size, np.int32)
+        self.reward_memory = np.zeros(self.memory_size, np.float32)
+        self.terminal_memory = np.zeros(self.memory_size, np.bool8)
+
+    def store_transition(
+        self,
+        state: np.ndarray,
+        action: int,
+        reward: float,
+        next_state: np.ndarray,
+        terminal: bool,
+    ) -> None:
+        """Stores a transition in the replay buffer.
+
+        Parameters
+        ----------
+        state: np.ndarray
+            The current state.
+        action: int
+            The action taken.
+        reward: float
+            The reward received.
+        next_state: np.ndarray
+            The new state.
+        terminal: bool
+            Whether or not the episode is over.
+        """
+
+        index = self.memory_counter % self.memory_size
+        self.state_memory[index] = state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.next_state_memory[index] = next_state
+        self.terminal_memory[index] = terminal
+        self.memory_counter += 1
+
+    def sample_buffer(
+        self, batch_size: int
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        """Samples a batch from the replay buffer.
+
+        Parameters
+        ----------
+        batch_size: int
+            The size of the batch to sample.
+
+        Returns
+        -------
+        states: np.ndarray
+            The sampled states.
+        actions: np.ndarray
+            The sampled actions.
+        rewards: np.ndarray
+            The sampled rewards.
+        next_states: np.ndarray
+            The sampled next states.
+        terminals: np.ndarray
+            The sampled terminal flag for states.
+        """
+
+        max_memory = min(self.memory_counter, self.memory_size)
+        batch = np.random.choice(max_memory, batch_size, replace=False)
+        states = self.state_memory[batch]
+        next_states = self.next_state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        terminals = self.terminal_memory[batch]
+        return states, actions, rewards, next_states, terminals
